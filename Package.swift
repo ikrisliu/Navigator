aa7843f44322fb4d34566af376f73ce100faff19
Kris <ikris.liu@gmail.com>
2019-11-01 19:22:21 +0800

    Update Package.swift
    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Navigator",
+    platforms: [
+        .iOS(.v10),
+    ],
     products: [
         .library(
             name: "Navigator",
